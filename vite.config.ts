3e7b2375f206665ebbd056165262c2f1b3076210
nathaniel <nathanielsalao13@gmail.com>
2026-04-09 10:00:33 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore
 import tailwindcss from '@tailwindcss/vite'
-
 
 export default defineConfig({
   plugins: [
